faf084e4f002b18b6a0fee5334cdc1563804125d
unknown <sanu.sanata@gmail.com>
2020-03-02 22:23:58 +0530

    First Commit

---
+++ test.py
@@ -0,0 +1,19 @@
+
+#def masq():
+  #import math
+  #print(math.sqrt(9)) 
+
+  #a = 2.3
+  #print math.floor(a)
+
+
+
+
+
+
+
+
+from random import *
+#for i in range(10):
+print(randint(0,9),randint(0,9))
+#print(randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),sep" ")
